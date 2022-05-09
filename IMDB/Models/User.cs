4740f8a233eb4d4b1e24a51c457f0ecf71e088bd
yasser74220 <yasserahlawy970@gmail.com>
2022-05-09 02:04:54 +0200

    New Commit

--- IMDB/Models/User.cs
+++ IMDB/Models/User.cs
@@ -10,10 +10,57 @@ namespace IMDB.Models
     {
         [Key]
         public int User_ID { get; set; }
-        public String Email{ get; set; }
-        public String Passward { get; set; }
-        public int Role_ID { get; set; }
-        public byte[] Img { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string Fname { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Second Name")]
+        public string Lname { get; set; }
+
+        [FileExtensions(Extensions = "jpg,jpeg,png")]
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "User Image")]
+        public string Image { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Email Address")]
+        public String Email{ get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Compare("Email", ErrorMessage = "The email and confirmation email do not match.")]
+        public String ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public String ConfirmPassword { get; set; }
+
+        [Required]
+        public UserGender Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Birthday { get; set; }
+
+        
+
+    }
+
+    public enum UserGender
+    {
+        Male,
+        Female
     }
 }
