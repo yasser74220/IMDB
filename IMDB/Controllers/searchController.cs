05497adfe271222976e6b970bef50da3fe4bbd20
Mohamed-76 <62908253+Mohamed-76@users.noreply.github.com>
2022-05-11 22:21:47 +0200

    new search with out style

---
+++ IMDB/Controllers/searchController.cs
@@ -0,0 +1,50 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IMDB.DataLayer;
+using IMDB.ViewModels;
+using System.Data.Entity;
+
+namespace IMDB.Controllers
+{
+    public class searchController : Controller
+    {
+
+        IMDBContext _context = new IMDBContext();
+        SearchViewModel searchVM = new SearchViewModel();
+        // GET: Search
+        [HttpGet]
+        public ActionResult Search()
+        {
+            searchVM.Actors = _context.Actors.ToList();
+            searchVM.Directors = _context.Directors.ToList();
+            searchVM.Movies = _context.Movies.ToList();
+            return View(searchVM);
+        }
+
+
+        [HttpPost]
+        public ActionResult Search(string SearchValue)
+        {
+            if (SearchValue == null)
+            {
+
+            }
+            else
+            {
+
+                string[] SearchSplit = SearchValue.Split(' ');
+                foreach (var item in SearchSplit)
+                {
+                    searchVM.Actors = _context.Actors.Where(ActorModel => ActorModel.FirstName.StartsWith(item) || ActorModel.LastName.StartsWith(item) || SearchValue == null);
+                    searchVM.Directors = _context.Directors.Where(DirectorModel => DirectorModel.FirstName.StartsWith(item) || DirectorModel.LastName.StartsWith(item) || SearchValue == null);
+                    searchVM.Movies = _context.Movies.Where(MovieModel => MovieModel.Movie_Name.StartsWith(item) || SearchValue == null);
+                }
+            }
+            return View(searchVM);
+        }
+    }
+}
---
+++ IMDB/ViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IMDB.Models;
+
+namespace IMDB.ViewModels
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<Actor> Actors { get; set; }
+        public IEnumerable<Movie> Movies { get; set; }
+        public IEnumerable<Director> Directors { get; set; }
+    }
+}
